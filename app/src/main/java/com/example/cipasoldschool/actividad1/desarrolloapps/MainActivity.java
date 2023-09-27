e4310d2d24a10342c14bcfd61ec93d85ea5df70c
Edwin de Jesus Marrugo Rodriguez <edwin.marrugo@pragma.com.co>
2023-09-26 23:04:37 -0500

    initial commit

---
+++ app/src/main/java/com/example/cipasoldschool/actividad1/desarrolloapps/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cipasoldschool.actividad1.desarrolloapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
